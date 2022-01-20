1d72123e9b585670fedc5da61c7186b51fef3517
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-20 15:43:54 -0800

    fixed tailwind

--- pages/home.js
+++ pages/home.js
@@ -4,8 +4,9 @@ function Home() {
   return (
     <div>
       <header>
-        <div>
+        <div className="relative w-36 h-10">
           <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </div>
       </header>
     </div>
