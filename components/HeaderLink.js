0c79fd687e4e978b8843d9f179f8cc625aad2566
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-20 16:12:23 -0800

    created headerlink component

---
+++ components/HeaderLink.js
@@ -0,0 +1,5 @@
+function HeaderLink() {
+  return <div>adfadfad</div>;
+}
+
+export default HeaderLink;
--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HeaderLink from "../components/HeaderLink";
 
 function Home() {
   return (
     <div>
-      <header>
+      <header className="flex justify-around items-center py-4">
         {/* Left Section */}
         <div className="relative w-36 h-10">
           <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
@@ -12,7 +13,13 @@ function Home() {
         {/* Right section (icons) */}
         <div className="flex items-center sm:divide-x divide-gray-300">
           {/* contains links */}
-          <div className="hidden sm:flex space-x-8 pr-4">{/* <HeaderLink /> */}</div>
+          <div className="hidden sm:flex space-x-8 pr-4">
+            {/* <HeaderLink /> */}
+            <HeaderLink />
+            <HeaderLink />
+            <HeaderLink />
+            <HeaderLink />
+          </div>
         </div>
       </header>
     </div>
