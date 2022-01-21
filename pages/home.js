bb13463fc6b37b528cecb2eee9960eebc71a48ab
Luis Valdivia <luis.edvaldivia@gmail.com>
2022-01-20 16:08:34 -0800

    styled

--- pages/home.js
+++ pages/home.js
@@ -4,8 +4,15 @@ function Home() {
   return (
     <div>
       <header>
+        {/* Left Section */}
         <div className="relative w-36 h-10">
           <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
+        </div>
+
+        {/* Right section (icons) */}
+        <div className="flex items-center sm:divide-x divide-gray-300">
+          {/* contains links */}
+          <div className="hidden sm:flex space-x-8 pr-4">{/* <HeaderLink /> */}</div>
         </div>
       </header>
     </div>
