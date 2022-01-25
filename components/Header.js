110a8bba5ba9eba0f20886adcc1675338e21c10b
levaldiv <luis.edvaldivia@gmail.com>
2022-01-24 18:09:48 -0800

    created header comp

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      {/* Left */}
+      <div className="flex items-center space-x-2 w-full max-w-xs">
+        <Image />
+      </div>
+
+      {/* Right */}
+    </header>
+  );
+}
+
+export default Header;
