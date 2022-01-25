3a04f20b4bcd34eb76f959076bbb7eb4b560b5ac
levaldiv <luis.edvaldivia@gmail.com>
2022-01-24 18:02:30 -0800

    added authorization to ensure users can pick an acct

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,6 +9,11 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+        },
+      },
     }),
     // ...add more providers here
   ],
