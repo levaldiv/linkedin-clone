584c83f72754c93f999a04da7256f49a11b39e93
levaldiv <luis.edvaldivia@gmail.com>
2022-01-25 13:02:18 -0800

    added search bar icon/input

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,30 @@
 import Image from "next/image";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import HeaderLink from "./HeaderLink";
+import GroupIcon from "@mui/icons-material/Group";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
 
 function Header() {
   return (
     <header>
       {/* Left */}
       <div className="flex items-center space-x-2 w-full max-w-xs">
-        <Image />
+        <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+
+        {/* Searchbar / icon / input field */}
+        <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
+          <SearchRoundedIcon />
+          <input
+            type="text"
+            placeholder="Search"
+            // hidden on phones, outline removes the outline around the box when selected
+            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow-1"
+          />
+        </div>
       </div>
 
       {/* Right */}
