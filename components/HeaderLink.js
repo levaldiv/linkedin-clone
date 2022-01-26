e3d1c3801a0cc9bb940d583af470ae7ae1806a9a
levaldiv <luis.edvaldivia@gmail.com>
2022-01-26 11:53:45 -0800

    customized props (active, hidden, feed)

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,14 +1,24 @@
-function HeaderLink({ Icon, text, avatar, feed }) {
+function HeaderLink({ Icon, text, feed, active, avatar, hidden }) {
   return (
     <div
-      className={`cursor-pointer flex flex-col justify-center items-center ${
+      // only on medium screens , i want it to be inline flex and hide certain icons
+      className={`${
+        hidden && "hidden md:inline-flex"
+      } cursor-pointer flex flex-col justify-center items-center ${
         feed
           ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1"
           : "text-gray-500 hover:text-gray-700"
-      }`}
+        // if active is true, i want to change colors (using ! to override original text)
+      } ${active && "!text-black dark:!text-white"}`}
     >
       {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
-      <h4 className="text-sm">{text}</h4>
+      <h4
+        className={`text-sm ${
+          feed && "hidden lg:flex justify-center w-full mx-auto"
+        }`}
+      >
+        {text}
+      </h4>
     </div>
   );
 }
