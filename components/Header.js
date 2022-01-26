dddc1bb25632525016c65fdd4800fadbda55c249
levaldiv <luis.edvaldivia@gmail.com>
2022-01-26 11:30:19 -0800

    added links to header comp

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,7 @@ import ChatIcon from "@mui/icons-material/Chat";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import { Avatar } from "@mui/material";
 
 function Header() {
   return (
@@ -27,7 +28,16 @@ function Header() {
         </div>
       </div>
 
-      {/* Right */}
+      {/* Right side */}
+      <div className="flex items-center space-x-6">
+        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+        <HeaderLink Icon={GroupIcon} text="My Network" feed />
+        <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+        <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+        <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
+        <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+        <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+      </div>
     </header>
   );
 }
