0c97e21d1e31f3e6456768d6aa22cc77ced7ccba
levaldiv <luis.edvaldivia@gmail.com>
2022-01-27 14:51:23 -0800

    changed tab title

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,9 @@
 function Sidebar() {
-  return <div></div>;
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
 }
 
 export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>LinkedIn</title>
+        <title>Feed | LinkedIn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
