d276eb7bd667935ac5307d9e68373cfb90062982
levaldiv <luis.edvaldivia@gmail.com>
2022-01-27 14:36:15 -0800

    imported sidebar to index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
@@ -13,7 +14,9 @@ export default function Home() {
       {/* Only use space when using in one direction, gap for horizontal and vertical */}
       <main className="flex justify-center gap-x-5 px-4 sm:px-12">
         <div className="flex flex-col md:flex-row gap-5">
+
           {/* Sidebar */}
+          <Sidebar />
           {/* Feed */}
         </div>
         {/* Widgets */}
