13c054ae30574479da37df2e76eb6091149c6b59
levaldiv <luis.edvaldivia@gmail.com>
2022-01-28 15:20:09 -0800

    wrapped app in session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
