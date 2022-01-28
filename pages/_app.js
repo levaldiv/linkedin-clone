bdce5a231cc0c0650722612204d29bf8d382ec0d
levaldiv <luis.edvaldivia@gmail.com>
2022-01-28 15:21:41 -0800

    exposing session context

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class">
