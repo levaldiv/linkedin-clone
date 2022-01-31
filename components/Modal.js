3b3c7b29255b076eb2973f8bde62aa509a703f52
levaldiv <luis.edvaldivia@gmail.com>
2022-01-31 11:38:11 -0800

    created animation for posting

--- components/Modal.js
+++ components/Modal.js
@@ -1,9 +1,53 @@
-function Modal() {
-  return (
-    <div>
-      <h1></h1>
-    </div>
-  );
-}
+import { useSession } from "next-auth/react";
+import { useRecoilValue } from "recoil";
 
-export default Modal;
+/********** Creating the animation **********/
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      type: "spring",
+      damping: 25,
+      stiffness: 500,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
+const gifYouUp = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.2,
+      ease: "easeIn",
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 0.15,
+      ease: "easeOut",
+    },
+  },
+};
+/********************************************/
+
+const Modal = ({ handleClose, type }) => {
+  const { data: session } = useSession();
+  const post = useRecoilValue(getPostState);
+};
