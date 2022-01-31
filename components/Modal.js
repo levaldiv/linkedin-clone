a532d7bcac438b82ff061975dc2b9f68b69b5b19
levaldiv <luis.edvaldivia@gmail.com>
2022-01-31 11:55:34 -0800

    styled the divs

--- components/Modal.js
+++ components/Modal.js
@@ -103,8 +103,13 @@ const Modal = ({ handleClose, type }) => {
             src={post.photoUrl}
             className="object-contain max-h-[80vh] w-full max-w-3xl rounded-l-lg"
           />
+          <div className="w-full md:w-3/5 bg-white dark:bg-[#1D2226] rounded-r-lg">
+            <Post post={post} modalPost />
+          </div>
         </motion.div>
       )}
     </Backdrop>
   );
 };
+
+export default Modal;
