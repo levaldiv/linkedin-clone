ba2e86b9f722b6b68107eb6217938e91c9e15bc3
levaldiv <luis.edvaldivia@gmail.com>
2022-01-31 10:48:32 -0800

    added onclick for the post btn

--- components/Input.js
+++ components/Input.js
@@ -26,10 +26,10 @@ function Input() {
           whileHover={{ scale: 1.01 }}
           whileTap={{ scale: 0.99 }}
           className="rounded-full border border-gray-400 py-2.5 px-3 opacity-80 hover:opacity-100 font-medium w-full text-left"
-          //   onClick={() => {
-          //     setModalOpen(true);
-          //     setModalType("dropIn");
-          //}}
+          onClick={() => {
+            setModalOpen(true);
+            setModalType("dropIn");
+          }}
         >
           Start a post
         </motion.button>
