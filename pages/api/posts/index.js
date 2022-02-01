2448df162406b83e1b81aeb479bdf2dca27d1093
levaldiv <luis.edvaldivia@gmail.com>
2022-02-01 15:43:14 -0800

    added GET/POST methods for posts

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,3 +1,4 @@
+import { Timestamp } from "mongodb";
 import { connectToDatabase } from "../../../lib/mongodb";
 
 export default async function handler(req, res) {
@@ -6,4 +7,25 @@ export default async function handler(req, res) {
 
   //accessing db instance
   const { db } = await connectToDatabase();
+
+  //   Use a GET request to fetch the data from the database
+  if (method === "GET") {
+    try {
+      //   fetching all our posts
+      const posts = await db.collection;
+    } catch (error) {}
+  }
+
+  //   To send a post to the database, we would use a POST request
+  if (method === "POST") {
+    try {
+      /* fetching all our posts
+       * Access the collection, create a collection called "posts"
+       * Insert one document (passing in body with spread operator to combine whatever the body contains)
+       * Combine body + timestamp (not actually sending the timestamp so use new ts) */
+      const post = await db
+        .collection("posts")
+        .insertOne({ ...body, timestamp: new Timestamp() });
+    } catch (error) {}
+  }
 }
