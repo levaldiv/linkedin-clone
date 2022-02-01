e8e26632ccae133e2bfd521ed872aea07a914cea
levaldiv <luis.edvaldivia@gmail.com>
2022-01-31 17:57:37 -0800

    created post btn

--- components/Form.js
+++ components/Form.js
@@ -1,12 +1,18 @@
+// import { useSession } from "next-auth/react";
 import { useState } from "react";
+// import { useRecoilState } from "recoil";
+// import { modalState } from "../atoms/modalAtom";
+// import { handlePostState } from "../atoms/postAtom";
 
 function Form() {
   /* setting whatever the user is typing to the state to store what they are typing
    * by default it is empty */
   const [input, setInput] = useState("");
-  
+  //   console.log(input);
+  const [photoUrl, setPhotoUrl] = useState("");
+
   return (
-    <form>
+    <form className="flex flex-col relative space-y-2 text-black/80 dark:text-white/75">
       <textarea
         rows="4"
         placeholder="What do you want to talk about?"
@@ -17,6 +23,24 @@ function Form() {
         of the state */
         onChange={(e) => setInput(e.target.value)}
       />
+
+      {/* photo url */}
+      <input
+        type="text"
+        placeholder="Add a photo URL (optional)"
+        className="bg-transparent focus:outline-none truncate max-w-xs md:max-w-sm dark:placeholder-white/75"
+        value={photoUrl}
+        onChange={(e) => setPhotoUrl(e.target.value)}
+      />
+
+      <button
+        className="absolute bottom-0 right-0 font-medium bg-blue-400 hover:bg-blue-500 disabled:text-black/40 disabled:bg-white/75 disabled:cursor-not-allowed text-white rounded-full px-3.5 py-1"
+        type="submit"
+        // onClick={uploadPost}
+        // disabled={!input.trim() && !photoUrl.trim()}
+      >
+        Post
+      </button>
     </form>
   );
 }
