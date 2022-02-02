3cc1a059eafdf18b6e8d723e144cc1cb2463bb61
levaldiv <luis.edvaldivia@gmail.com>
2022-02-02 11:28:26 -0800

    fetching the posts

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import Input from "./Input";
 
 function Feed() {
+  // fetching posts from database
+  useEffect(() => {
+    const fetchPost = async () => {
+      // send our options
+      const response = await fetch("/api/posts/", {
+        // retrieve back the posts
+        method: "GET",
+        // retrieve the posts in json format
+        headers: { "Content-Type": "application/json" },
+      });
+
+      /* Recieve back the data
+       * Sending a response back from the backend (sneding a
+       * response back to the client from the backend)
+       * */
+      const responseData = await response.json();
+    };
+  }, []);
+
   return (
     <div className="space-x-6 pb-24 max-w-lg">
       <Input />
