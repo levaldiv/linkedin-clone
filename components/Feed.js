f43dc66b4c854de471a424e3e5498422b7896bd1
levaldiv <luis.edvaldivia@gmail.com>
2022-02-02 12:26:24 -0800

    added vars

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 import Input from "./Input";
+import { handlePostState, useSSRPostsState } from "../atoms/postAtom";
 
 function Feed() {
   const [realtimePosts, setRealtimePosts] = useState([]);
+  // Want thess as a global state to be stored asd global state so i cn have access to these vars
+  const [handlePost, setHandlePost] = useRecoilState(handlePostState);
+  // Use Server Side Rendered (SSR)
+  const [useSSRPost, setUseSSRPosts] = useRecoilState(useSSRPostsState);
 
   // fetching posts from database
   useEffect(() => {
@@ -27,7 +33,10 @@ function Feed() {
 
     // invoking the function
     fetchPosts();
-  }, []);
+
+    /* Only have a dependency if that dep changes then only its going to run the use effect again
+     * handlePost changes ONLY IF handlePost becomes true or false, then itll run the useEffect */
+  }, [handlePost]);
 
   return (
     <div className="space-x-6 pb-24 max-w-lg">
