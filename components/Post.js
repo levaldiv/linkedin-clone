9170858d77037bf97063799e5c9791f140424e44
levaldiv <luis.edvaldivia@gmail.com>
2022-02-03 14:52:14 -0800

    added profile pic

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,20 @@
-function Post({ post }) {
+import { Avatar } from "@mui/material";
+
+function Post({ post, modalPost }) {
   return (
-    <div>
-      <img src={post.photoUrl} alt="" />
-      <p>{post.input}</p>
+    <div
+      className={`bg-white dark:bg-[#1D2226] ${
+        modalPost ? "rounded-r-lg" : "rounded-lg"
+      } space-y-2 py-2.5 border border-gray-300 dark:border-none`}
+    >
+      <div className="flex items-center px-2.5 cursor-pointer">
+        <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer" />
+        <div className="mr-auto ml-2 leading-none">
+          <h6 className="font-medium hover:text-blue-500 hover:underline">
+            {post.username}
+          </h6>
+        </div>
+      </div>
     </div>
   );
 }
